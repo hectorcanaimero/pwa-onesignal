f795d0cfb2f0415f81209c28224beaad5ce29657
Hector Velasquez <rodriguez.hectoralejandro@gmail.com>
2020-06-25 17:18:22 -0300

    At

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  readonly VAPID_PUBLIC = 'BAgMf-FyRyN4nRYFbKlviIaX0ZzAjyIq5xjbzHRhMGkitBYAulUDxhYHF46c8m4nV7vR6Psa7aWjiIFQnGJX4hk';
+
   constructor(
+    private swPush: SwPush,
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
   ) {
     this.initializeApp();
   }
@@ -24,4 +29,10 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+//   subscribeToNotifications() {
+//     this.swPush.requestSubscription({ serverPublicKey: this.VAPID_PUBLIC })
+//     .then(sub => console.log(sub))
+//     .catch(err => console.error('Could not subscribe to notifications', err));
+// }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NgxOneSignalModule } from 'ngx-onesignal';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('OneSignalSDKWorker.js', { enabled: environment.production }),
+    NgxOneSignalModule.forRoot({ appId: 'f09c1767-31e2-4894-8b7f-66588f880318'}),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
